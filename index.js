c019f32e2a1763eddb6a6a7ccc050169c4e1d08e
Gideon Macharia <kamaugedion95@gmail.com>
2024-03-22 15:18:06 +0300

    add array methods

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+   return cats.push("Ralph")
+}
+
+// destructivelyAppendCat("Ralph")
+function destructivelyPrependCat(name) {
+    return cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat() {
+    return cats.pop()
+}
+function  destructivelyRemoveFirstCat() {
+    return cats.shift()
+}
+function appendCat(name) {
+    const copyOfCats = [...cats,"Broom"]
+     console.log(cats)
+     return copyOfCats
+}
+function prependCat(name) {
+    const copyOfCats = ["Arnold",...cats]
+     console.log(cats)
+     return copyOfCats
+}
+function  removeLastCat() {
+    return cats.slice(0, -1)
+}
+function removeFirstCat() {
+    return cats.slice(1)
+}
